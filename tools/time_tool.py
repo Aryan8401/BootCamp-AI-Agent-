c0452c774cfbbe986d03567af4856ccfb8af0d41
Aryan8401 <aryanjaiwal8401@gmail.com>
2026-07-16 14:33:18 +0530

    Update time tool to show IST time

--- tools/time_tool.py
+++ tools/time_tool.py
@@ -1,9 +1,12 @@
 from datetime import datetime
+from zoneinfo import ZoneInfo
+
+IST = ZoneInfo("Asia/Kolkata")
 
 
 def execute(argument: dict):
-    now = datetime.now()
-    return now.strftime("%d-%m-%Y %I:%M:%S %p")
+    now = datetime.now(IST)
+    return now.strftime("%d-%m-%Y %I:%M:%S %p") + " IST"
 
 
 if __name__ == "__main__":
